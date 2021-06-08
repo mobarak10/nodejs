65d44eb1bfaace1064fc73fe137ab6df4e146b08
mobarak10 <47492246+mobarak10@users.noreply.github.com>
2021-06-08 23:43:34 +0600

    modifie url

--- lesson-5/http.js
+++ lesson-5/http.js
@@ -1,7 +1,19 @@
 /* eslint-disable prettier/prettier */
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello Programmer!');
+        res.write('How are you doing?');
+        res.end();
+    } else if (req.url === '/about') {
+        res.write('This is about page');
+        res.end();
+    } else {
+        res.write('Not found!!!');
+        res.end();
+    }
+});
 
 server.listen(3000);
 
