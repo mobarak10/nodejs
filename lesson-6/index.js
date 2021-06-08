5da74f55a14ef741f2a89e744d37c428e926d932
mobarak10 <47492246+mobarak10@users.noreply.github.com>
2021-06-08 23:58:54 +0600

    work in file system & read file using toString

---
+++ lesson-6/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+const fs = require('fs');
+
+const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
+
+ourReadStream.on('data', (chunk) => {
+    console.log(chunk.toString());
+});
