472f5a7ff28e9ef21f26a0adbf7ac6941f6ffb17
mobarak10 <47492246+mobarak10@users.noreply.github.com>
2021-06-09 23:09:19 +0600

    working in node pipe

--- lesson-6/index.js
+++ lesson-6/index.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
 const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
 
-ourReadStream.on('data', (chunk) => {
-    ourWriteStream.write(chunk);
-});
+// ourReadStream.on('data', (chunk) => {
+//     ourWriteStream.write(chunk);
+// });
+
+ourReadStream.pipe(ourWriteStream);
