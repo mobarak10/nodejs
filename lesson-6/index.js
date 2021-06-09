4382bf11cf09f783fed5adb6e1cd4f16437d4a25
mobarak10 <47492246+mobarak10@users.noreply.github.com>
2021-06-09 18:23:27 +0600

    file structring

--- lesson-6/index.js
+++ lesson-6/index.js
@@ -1,36 +1,4 @@
 /* eslint-disable prettier/prettier */
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('<html><head><title></title></head>');
-        res.write(
-            '<body><form method="post" action="process"><input type="text" name="message" /></form></body>',
-            );
-        res.end();
-    } else if (req.url === '/process' && req.method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-            console.log('Strimed Finished');
-            const parseBody = Buffer.concat(body).toString();
-
-            console.log(parseBody);
-        });
-        res.write('Thank you for submitting');
-        res.end();
-    } else {
-        res.write('Not found!!!');
-        res.end();
-    }
-});
-
-server.listen(3000);
-
-console.log('server listening in 3000 port');
 
 // const fs = require('fs');
 
